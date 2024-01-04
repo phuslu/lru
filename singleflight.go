a9c01dd48c5c0a6394f2bb817ba700e83d3e5ef0
phuslu <phus.lu@gmail.com>
2024-01-04 21:58:14 +0800

    slim singleflight

--- singleflight.go
+++ singleflight.go
@@ -18,8 +18,7 @@ type singleflight_call[T any] struct {
 	// These fields are read and written with the singleflight
 	// mutex held before the WaitGroup is done, and are read but
 	// not written after the WaitGroup is done.
-	dups  int
-	chans []chan<- singleflight_Result[T]
+	dups int
 }
 
 // Group represents a class of work and forms a namespace in
@@ -62,30 +61,6 @@ func (g *singleflight_Group[K, V]) Do(key K, fn func() (V, error)) (v V, err err
 	return c.val, c.err, c.dups > 0
 }
 
-// DoChan is like Do but returns a channel that will receive the
-// results when they are ready.
-func (g *singleflight_Group[K, V]) DoChan(key K, fn func() (V, error)) <-chan singleflight_Result[V] {
-	ch := make(chan singleflight_Result[V], 1)
-	g.mu.Lock()
-	if g.m == nil {
-		g.m = make(map[K]*singleflight_call[V])
-	}
-	if c, ok := g.m[key]; ok {
-		c.dups++
-		c.chans = append(c.chans, ch)
-		g.mu.Unlock()
-		return ch
-	}
-	c := &singleflight_call[V]{chans: []chan<- singleflight_Result[V]{ch}}
-	c.wg.Add(1)
-	g.m[key] = c
-	g.mu.Unlock()
-
-	go g.doCall(c, key, fn)
-
-	return ch
-}
-
 // doCall handles the single singleflight_call for a key.
 func (g *singleflight_Group[K, V]) doCall(c *singleflight_call[V], key K, fn func() (V, error)) {
 	c.val, c.err = fn()
@@ -93,27 +68,5 @@ func (g *singleflight_Group[K, V]) doCall(c *singleflight_call[V], key K, fn fun
 
 	g.mu.Lock()
 	delete(g.m, key)
-	for _, ch := range c.chans {
-		ch <- singleflight_Result[V]{c.val, c.err, c.dups > 0}
-	}
 	g.mu.Unlock()
-}
-
-// ForgetUnshared tells the singleflight to forget about a key if it is not
-// shared with any other goroutines. Future singleflight_calls to Do for a forgotten key
-// will singleflight_call the function rather than waiting for an earlier singleflight_call to complete.
-// Returns whether the key was forgotten or unknown--that is, whether no
-// other goroutines are waiting for the result.
-func (g *singleflight_Group[K, V]) ForgetUnshared(key K) bool {
-	g.mu.Lock()
-	defer g.mu.Unlock()
-	c, ok := g.m[key]
-	if !ok {
-		return true
-	}
-	if c.dups == 0 {
-		delete(g.m, key)
-		return true
-	}
-	return false
 }
