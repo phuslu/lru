426b1a44d167c5bbba07dd2f989c0c79d9967864
phuslu <phus.lu@gmail.com>
2024-01-04 15:24:36 +0800

    add comments to clock

--- shard.go
+++ shard.go
@@ -148,19 +148,20 @@ func newshard[K comparable, V any](size int) *shard[K, V] {
 	return s
 }
 
+// clock is the number of seconds since January 1, 2024 UTC
 var clock uint32
 
 func init() {
-	const unixBase = 1704067200 // 2024-01-01T00:00:00Z
-	atomic.StoreUint32(&clock, uint32(time.Now().Unix()-unixBase))
+	const clockBase = 1704067200 // 2024-01-01T00:00:00Z
+	atomic.StoreUint32(&clock, uint32(time.Now().Unix()-clockBase))
 	go func() {
 		for {
 			for i := 0; i < 9; i++ {
 				time.Sleep(100 * time.Millisecond)
-				atomic.StoreUint32(&clock, uint32(time.Now().Unix()-unixBase))
+				atomic.StoreUint32(&clock, uint32(time.Now().Unix()-clockBase))
 			}
 			time.Sleep(100 * time.Millisecond)
-			atomic.StoreUint32(&clock, uint32(time.Now().Unix()-unixBase))
+			atomic.StoreUint32(&clock, uint32(time.Now().Unix()-clockBase))
 		}
 	}()
 }
