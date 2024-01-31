1f71fb4d31aa897d43f777ee40fa5c34ecf1f903
phuslu <phus.lu@gmail.com>
2024-01-31 20:58:53 +0800

    local mask variable

--- shard_table.go
+++ shard_table.go
@@ -14,9 +14,9 @@ const (
 )
 
 func (s *shard[K, V]) table_Init(size uint32) {
-	sz := max(nextPowOf2(size), 8)
-	s.table.buckets = make([]struct{ hdib, index uint32 }, sz)
-	s.table.mask = uint32(len(s.table.buckets) - 1)
+	size = max(nextPowOf2(size), 8)
+	s.table.buckets = make([]struct{ hdib, index uint32 }, size)
+	s.table.mask = size - 1
 	s.table.length = 0
 }
 
@@ -25,7 +25,8 @@ func (s *shard[K, V]) table_Init(size uint32) {
 func (s *shard[K, V]) table_Set(hash uint32, key K, index uint32) (prev uint32, ok bool) {
 	subhash := hash >> dibBitSize
 	hdib := subhash<<dibBitSize | uint32(1)&maxDIB
-	i := (hdib >> dibBitSize) & s.table.mask
+	mask := s.table.mask
+	i := (hdib >> dibBitSize) & mask
 	for {
 		if s.table.buckets[i].hdib&maxDIB == 0 {
 			s.table.buckets[i].hdib = hdib
@@ -44,7 +45,7 @@ func (s *shard[K, V]) table_Set(hash uint32, key K, index uint32) (prev uint32, 
 			hdib, s.table.buckets[i].hdib = s.table.buckets[i].hdib, hdib
 			index, s.table.buckets[i].index = s.table.buckets[i].index, index
 		}
-		i = (i + 1) & s.table.mask
+		i = (i + 1) & mask
 		hdib = hdib>>dibBitSize<<dibBitSize | (hdib&maxDIB+1)&maxDIB
 	}
 }
@@ -75,7 +76,8 @@ func (s *shard[K, V]) table_Len() int {
 // Returns the deleted index, or false when no index was assigned.
 func (s *shard[K, V]) table_Delete(hash uint32, key K) (v uint32, ok bool) {
 	subhash := hash >> dibBitSize
-	i := subhash & s.table.mask
+	mask := s.table.mask
+	i := subhash & mask
 	for {
 		if s.table.buckets[i].hdib&maxDIB == 0 {
 			return
@@ -85,15 +87,16 @@ func (s *shard[K, V]) table_Delete(hash uint32, key K) (v uint32, ok bool) {
 			s.table_delete(i)
 			return old, true
 		}
-		i = (i + 1) & s.table.mask
+		i = (i + 1) & mask
 	}
 }
 
 func (s *shard[K, V]) table_delete(i uint32) {
+	mask := s.table.mask
 	s.table.buckets[i].hdib = s.table.buckets[i].hdib>>dibBitSize<<dibBitSize | uint32(0)&maxDIB
 	for {
 		pi := i
-		i = (i + 1) & s.table.mask
+		i = (i + 1) & mask
 		if s.table.buckets[i].hdib&maxDIB <= 1 {
 			s.table.buckets[pi].index = 0
 			s.table.buckets[pi].hdib = 0
